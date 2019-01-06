46b570bb57b8ea7190e70eddd3ae7bb39549fb0e
Andres Avendaño <afavendanor@unal.edu.co>
2019-01-06 18:58:17 -0500

    Servicio rest en Spring boot y base de datos H2

---
+++ ejemplo/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO CLIENTE(id, nombre, salario, bonificacion, fecha_ingreso, activo) VALUES('10445', 'Andres Avendaño', 1200, 300, SYSDATE(), true);
+INSERT INTO CLIENTE(id, nombre, salario, bonificacion, fecha_ingreso, activo) VALUES('43205', 'Lucas Silva', 3420, 230, SYSDATE(), false);
+INSERT INTO CLIENTE(id, nombre, salario, bonificacion, fecha_ingreso, activo) VALUES('87340', 'Ana Lopez', 2340, 100, SYSDATE(), true);
+INSERT INTO CLIENTE(id, nombre, salario, bonificacion, fecha_ingreso, activo) VALUES('83564', 'Sara Henao', 5430, 300, SYSDATE(), false);
+INSERT INTO CLIENTE(id, nombre, salario, bonificacion, fecha_ingreso, activo) VALUES('63875', 'Luis Perez', 4300, 600, SYSDATE(), true);
