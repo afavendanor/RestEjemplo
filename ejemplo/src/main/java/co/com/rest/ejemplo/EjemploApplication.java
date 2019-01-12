ba403606b6a41699c22b834137aa3b5cc0bb1ee0
Andres Avendaño <afavendanor@unal.edu.co>
2019-01-12 00:59:51 -0500

    Se agregan elementos para conexion a BD MySql

--- ejemplo/src/main/java/co/com/rest/ejemplo/EjemploApplication.java
+++ ejemplo/src/main/java/co/com/rest/ejemplo/EjemploApplication.java
@@ -1,8 +1,10 @@
 package co.com.rest.ejemplo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAutoConfiguration
 @SpringBootApplication
 public class EjemploApplication {
 
