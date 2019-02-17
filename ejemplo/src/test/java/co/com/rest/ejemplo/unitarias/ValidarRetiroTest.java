4c4353bd4f95880e5a3221fc7eafa2687747f12d
ANDRES AVENDAÑO <andres.avendano@ceiba.com.co>
2019-02-16 20:31:24 -0500

    Se implementan pruebas

--- ejemplo/src/test/java/co/com/rest/ejemplo/unitarias/ValidarRetiroTest.java
+++ ejemplo/src/test/java/co/com/rest/ejemplo/unitarias/ValidarRetiroTest.java
@@ -1,30 +1,52 @@
 package co.com.rest.ejemplo.unitarias;
 
-import static org.mockito.Mockito.times;
-
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 
 import co.com.rest.ejemplo.domain.ValidarRetiro;
+import co.com.rest.ejemplo.exception.ApplicationException;
 
 public class ValidarRetiroTest {
 
-	private static double SALDO_MINIMO = 100;
+	private static double SALDO = -1;
+	private static double MONTO_MAYOR = 6000;
+	private static double MONTO_NEGATIVO = -100;
 	private static double VALOR_MAXIMO_TRANSFERENCIA = 5000;
-	ValidarRetiro validarRetiroMock;
+	ValidarRetiro validarRetiro;
 	
 
 	@Before
-	public void mocksInitialization() {
-		MockitoAnnotations.initMocks(this);
-		validarRetiroMock = ValidarRetiro.getInstance();
+	public void Initialization() {
+		validarRetiro = ValidarRetiro.getInstance();
+	}
+
+	@Test
+	public void validarMontoNegativoTest() {		
+		ValidarRetiro.validarMontoNegativo(1200);
+	}
+	
+	@Test(expected = ApplicationException.class)
+	public void validarMontoNegativoExceptionTest() {		
+		ValidarRetiro.validarMontoNegativo(SALDO);
+	}
+	
+	@Test
+	public void validarMontoTransferenciaTest() {		
+		ValidarRetiro.validarMontoTransferencia(MONTO_NEGATIVO, VALOR_MAXIMO_TRANSFERENCIA);
+	}
+	
+	@Test(expected = ApplicationException.class)
+	public void validarMontoTransferenciaExceptionTest() {		
+		ValidarRetiro.validarMontoTransferencia(MONTO_MAYOR, VALOR_MAXIMO_TRANSFERENCIA);
 	}
 
 	@Test
 	public void validarSaldoMinimoTest() {		
-		Mockito.verify(validarRetiroMock).validarMontoNegativo(Mockito.anyDouble());
+		ValidarRetiro.validarSaldoMinimo(MONTO_MAYOR);
 	}
-
+	
+	@Test(expected = ApplicationException.class)
+	public void validarSaldoMinimoExceptionTest() {		
+		ValidarRetiro.validarSaldoMinimo(MONTO_NEGATIVO);
+	}
 }
